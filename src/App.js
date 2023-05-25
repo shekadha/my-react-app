0e6ca08dd244d9c4b317a926ceb9f5ae209a3178
shekadha <124559256+shekadha@users.noreply.github.com>
2023-05-25 08:18:49 +0330

    added github url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">weather app</header>
+      <header className="App-header">
+        <h1> weather app</h1>
+
+        <footer>
+          This code is created by Shekoufeh and is {""}
+          <a
+            href="https://github.com/shekadha/my-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </header>
     </div>
   );
 }
